170a2369e648998c667bb795c26ca4a39e853457
ShemMwanza <nzamba.shem@gmail.com>
2023-05-14 22:49:09 +0300

    theme

---
+++ reactnative-project/src/core/theme.js
@@ -0,0 +1,14 @@
+import { DefaultTheme } from 'react-native-paper'
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: '#000000',
+    primary: '#2dd27b',
+    secondary: '#414757',
+    error: '#f13a59',
+    white: '#ffffff',
+    grey: '#777777',
+  },
+}
