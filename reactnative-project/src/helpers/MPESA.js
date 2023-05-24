890bb57a8209c3bd770c76e7ae294b1f0de5486e
ShemMwanza <nzamba.shem@gmail.com>
2023-05-24 22:23:02 +0300

    Mpesa integration, no callback

--- reactnative-project/src/helpers/MPESA.js
+++ reactnative-project/src/helpers/MPESA.js
@@ -1,56 +1,68 @@
-import { REACT_APP_consumerKey, REACT_APP_consumerSecret } from '../Keys';
+import { REACT_APP_consumerKey, REACT_APP_consumerSecret } from '../../firebase/keys';
 import axios from 'axios';
-window.Buffer = window.Buffer || require("buffer").Buffer;
+import { encode, decode } from 'base-64';
 
 const consumer_key = REACT_APP_consumerKey;
 const consumer_secret = REACT_APP_consumerSecret;
 
+const encodeBase64 = (str) => {
+    return encode(str);
+};
+
 const LipaNaMpesa = async (phone_number) => {
-    const proxy = "https://proxy.cors.sh";
-    const auth = new Buffer.from(`${consumer_key}:${consumer_secret}`).toString("base64");
-
     try {
+        const timestamp = new Date().toISOString().replace(/[^0-9]/g, '').slice(0, -3); // Generate timestamp in the format YYYYMMDDHHMMSS
+        const auth = encodeBase64(`${consumer_key}:${consumer_secret}`);
 
-        const tokenData = await axios.get(
-            `${proxy}/https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials`,
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    authorization: `Basic ${auth}`,
-                },
-            }
-        );
+        // Step 1: Generate access token
+        const tokenResponse = await axios.get('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Basic ${auth}`,
+            },
+        });
 
-        let headers = new Headers();
-        headers.append("Content-Type", "application/json");
-        headers.append("Authorization", `Bearer ${tokenData.data.access_token}`);
+        const accessToken = tokenResponse.data.access_token;
 
-        fetch(proxy + "/https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest", {
-            method: 'POST',
-            headers,
-            body: JSON.stringify({
-                "BusinessShortCode": 174379,
-                "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjMwMTIyMjI1MTE1",
-                "Timestamp": "20230122225115",
-                "TransactionType": "CustomerPayBillOnline",
-                "Amount": 1,
-                "PartyA": phone_number,
-                "PartyB": 174379,
-                "PhoneNumber": phone_number,
-                "CallBackURL": "https://mydomain.com/path",
-                "AccountReference": "React-Firebase Project",
-                "TransactionDesc": "Buy Goods and Services",
-                "CheckoutRequestID": "ws_CO_DMZ_123212312_2342347678234"
-            })
-        }
-        ).catch(() => {
-            alert("There is a problem, try again later!")
-        })
+        // Step 2: Initiate Lipa Na M-Pesa Express request
+        const payload = {
+            BusinessShortCode: 174379,
+            Password: generateLipaNaMpesaPassword(timestamp),
+            Timestamp: timestamp,
+            TransactionType: 'CustomerPayBillOnline',
+            Amount: 1,
+            PartyA: phone_number,
+            PartyB: 174379,
+            PhoneNumber: phone_number,
+            CallBackURL: 'https://example.com/callback',
+            AccountReference: 'React-Firebase Project',
+            TransactionDesc: 'Buy Goods and Services',
+            CheckoutRequestID: 'ws_CO_DMZ_123212312_2342347678234',
+        };
+
+        const response = await axios.post('https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest', payload, {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+
+        console.log(response.data); // Handle the response data here
+
     } catch (error) {
         console.log(error);
-        alert(error.message);
+        throw new Error('Failed to initiate Lipa Na M-Pesa request');
     }
-}
+};
+
+// Function to generate Lipa Na M-Pesa password
+const generateLipaNaMpesaPassword = (timestamp) => {
+    const passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
+    const shortcode = 174379;
+    const password = encodeBase64(`${shortcode}${passkey}${timestamp}`);
+    return password;
+};
+
 export {
     LipaNaMpesa
-}
+};
