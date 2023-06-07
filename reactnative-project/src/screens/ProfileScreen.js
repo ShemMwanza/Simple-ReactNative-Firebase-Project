0e1a40b31d9d2e7b57c0168df49ed30710ea9029
ShemMwanza <nzamba.shem@gmail.com>
2023-06-07 14:39:13 +0300

    profile update

--- reactnative-project/src/screens/ProfileScreen.js
+++ reactnative-project/src/screens/ProfileScreen.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import ProfilePhoto from '../components/User/ProfilePhoto';
 import { useAuth } from '../../Context/AuthContext';
 import ProfileForm from '../components/User/ProfileForm';
-
+import Background from '../components/Background';
 const ProfileScreen = () => {
   const { currentUser } = useAuth();
 
   const image = currentUser?.photoURL ? currentUser.photoURL : 'https://picsum.photos/200/300';
   return (
-    <View>
-      <ProfilePhoto photoUrl={
-       image
-      } /> 
-      <ProfileForm />
-    </View>
+    <Background>
+      <View style={styles.container}>
+        <ProfilePhoto photoUrl={
+          image
+        } />
+        <ProfileForm />
+      </View>
+    </Background>
   );
 };
 
 export default ProfileScreen;
+
+const styles = StyleSheet.create({
+
+  container: {
+    marginTop: 16,
+    padding: 16,
+    borderRadius: 4,
+    flex: 1
+
+  },
+});
