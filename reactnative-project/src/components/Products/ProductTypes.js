a867a3d4a9213b376558495400f3970e5b698461
ShemMwanza <nzamba.shem@gmail.com>
2023-06-07 14:38:28 +0300

    product type

--- reactnative-project/src/components/Products/ProductTypes.js
+++ reactnative-project/src/components/Products/ProductTypes.js
@@ -15,12 +15,12 @@ const ProductCard = ({ name, image }) => {
 
 const ProductTypes = () => {
     const products = [
-        { name: 'Product 1', image: 'https://example.com/product1.jpg' },
-        { name: 'Product 2', image: 'https://example.com/product2.jpg' },
-        { name: 'Product 3', image: 'https://example.com/product3.jpg' },
-        { name: 'Product 4' },
-        { name: 'Product 5', image: 'https://example.com/product5.jpg' },
-        { name: 'Product 6' },
+        { name: 'Category 1', image: 'https://cdn-icons-png.flaticon.com/128/1302/1302480.png' },
+        { name: 'Category 2', image: 'https://cdn-icons-png.flaticon.com/128/1300/1300927.png' },
+        { name: 'Category 3', image: 'https://cdn-icons-png.flaticon.com/128/289/289872.png' },
+        { name: 'Category 4', image: 'https://cdn-icons-png.flaticon.com/128/7948/7948580.png' },
+        { name: 'Category 5', image: 'https://cdn-icons-png.flaticon.com/128/7399/7399938.png' },
+        { name: 'Category 6', image: 'https://cdn-icons-png.flaticon.com/128/11006/11006874.png' },
     ];
 
     const rows = [];
@@ -42,10 +42,9 @@ const ProductTypes = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        alignItems: 'stretch',
-
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 12,
     },
     row: {
         flexDirection: 'row',
