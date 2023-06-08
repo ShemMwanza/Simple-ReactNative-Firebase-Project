0dfcb098dd5e9d15f587eac478191f2193e237de
ShemMwanza <nzamba.shem@gmail.com>
2023-06-08 23:27:00 +0300

    Update

--- reactnative-project/App.js
+++ reactnative-project/App.js
@@ -1,20 +1,110 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Provider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { theme } from './src/core/theme';
+import {
+  LoginScreen,
+  RegisterScreen,
+  Dashboard,
+  ResetPasswordScreen,
+  StartScreen,
+  CheckoutScreen,
+} from './src/screens';
+import { AuthProvider } from './Context/AuthContext';
+import OptionsScreen from './src/screens/OptionsScreen';
+import AppLoading from 'expo-app-loading';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import PaymentMethodScreen from './src/screens/PaymentMethodScreen';
+import SpecificProductScreen from './src/screens/SpecificProductScreen';
+import MpesaPaymentScreen
+  from './src/screens/MpesaPaymentScreen';
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <Provider theme={theme}>
+        <StatusBar mode='light' />
+        <NavigationContainer>
+          <Stack.Navigator
+            // initialRouteName="StartScreen"
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name="StartScreen" component={StartScreen} />
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+            <Stack.Screen
+              name="ResetPasswordScreen"
+              component={ResetPasswordScreen}
+            />
+            <Stack.Screen name="Dashboard" component={Dashboard} options={{ headerShown: false }} />
+            <Stack.Screen
+              name="CheckoutScreen"
+              component={CheckoutScreen}
+              options={{
+                headerShown: true,
+                headerStyle: {
+                  backgroundColor: theme.colors.primary,
+                },
+                headerTintColor: 'white',
+              }}
+            />
+            <Stack.Screen
+              name="OptionsScreen"
+              component={OptionsScreen}
+              options={{
+                presentation: 'modal',
+                title: 'Options',
+                headerShown: true,
+                headerStyle: {
+                  backgroundColor: theme.colors.primary,
+                },
+                headerTintColor: 'white',
+              }}
+            />
+            <Stack.Screen
+              name="PaymentMethodScreen"
+              component={PaymentMethodScreen}
+              options={{
+                presentation: 'modal',
+                title: 'Payment Method',
+                headerShown: true,
+                headerStyle: {
+                  backgroundColor: theme.colors.primary,
+                },
+                headerTintColor: 'white',
+              }}
+            />
+            <Stack.Screen
+              name="SpecificProductScreen"
+              component={SpecificProductScreen}
+              options={{
+                headerStyle: {
+                  backgroundColor: theme.colors.primary,
+                },
+                headerTintColor: 'white',
+              }}
+            />
+            <Stack.Screen
+              name="MpesaPaymentScreen"
+              component={MpesaPaymentScreen}
+              options={{
+                headerShown: false,
+
+                headerStyle: {
+                  backgroundColor: theme.colors.primary,
+                },
+                headerTintColor: 'white',
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    </AuthProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
